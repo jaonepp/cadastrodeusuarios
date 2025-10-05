fefa34f30a547e067dc2a05ad5758985eec9ebcb
Juan <juanmarquesz145@gmail.com>
2025-10-05 00:57:54 -0300

    Mais verificações

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
 
+import org.example.repository.UserDAO;
 
-
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,7 +12,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        org.example.UserDAO user = new org.example.UserDAO();
+        UserDAO userDAO = new UserDAO();
 
         ArrayList<User> usuarios = new ArrayList<>();
 
@@ -20,61 +21,79 @@ public class Main {
             System.out.println("2 - Listar usuarios");
             System.out.println("3 - Buscar por nome");
             System.out.println("4 - sair");
+            System.out.println("5 - Gerar user aleatorio");
+            System.out.println("6 - Atualizar usuario");
             System.out.println("Escolha uma opção: ");
             int opcao = scanner.nextInt();
             scanner.nextLine();
 
-                    if (opcao == 1) {
-                        System.out.println("Nome: ");
-                        String nome = scanner.nextLine();
+            if (opcao == 1) {
+                System.out.println("Nome: ");
+                String nome = scanner.nextLine();
 
-                        while (nome.strip().isEmpty() || !nome.matches("^[A-Za-zÀ-ÿ\\s]+$")) {
-                            System.out.println("Nome inválido! Use apenas letras:");
-                            nome = scanner.nextLine();
-                        }
+                while (nome.strip().isEmpty() || !nome.matches("^[A-Za-zÀ-ÿ\\s]+$")) {
+                    System.out.println("Nome inválido! Use apenas letras:");
+                    nome = scanner.nextLine();
+                }
 
-                        System.out.println("Email: ");
-                        String email = scanner.nextLine();
-                        while (!email.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$")) {
-                            System.out.println("Email inválido. Tente novamente:");
-                            email = scanner.nextLine();
-                        }
+                System.out.println("Email: ");
+                String email = scanner.nextLine();
+                while (!email.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$")) {
+                    System.out.println("Email inválido. Tente novamente:");
+                    email = scanner.nextLine();
+                }
 
-                        boolean hasUser = user.hasEmail(email);
-                        if (hasUser) {
-                            System.out.println("Já existe um usuário com esse email!");
-                            continue;
-                        }
-                        System.out.println("idade: ");
-                        int idade = scanner.nextInt();
-                        while (idade <= 0 || idade > 100) {
-                            System.out.println("Idade invalida!");
-                            idade = scanner.nextInt();
-                            scanner.nextLine();
-                            }
+                boolean hasUser = userDAO.hasEmail(email);
+                if (hasUser) {
+                    System.out.println("Já existe um usuário com esse email!");
+                    continue;
+                }
+                System.out.println("idade: ");
+                int idade = scanner.nextInt();
+                while (idade <= 0 || idade > 100) {
+                    System.out.println("Idade invalida!");
+                    idade = scanner.nextInt();
+                    scanner.nextLine();
+                }
 
-                        user.adicionar(new User(nome, email, idade));
-                        System.out.println("Usuário cadastrado com sucesso!");
+                userDAO.adicionar(new User(nome, email, idade));
+                System.out.println("Usuário cadastrado com sucesso!");
 
-                    } else if (opcao == 2){
-                        System.out.println("Usuarios cadastrados: ");
-                        user.listar().forEach(System.out::println);
+            } else if (opcao == 2) {
+                System.out.println("Usuarios cadastrados: ");
+                userDAO.listar().forEach(System.out::println);
 
-                    } else if (opcao == 3){
-                        System.out.println("Digite o nome para busca: ");
-                        String busca = scanner.nextLine();
-                        var encontrados = user.buscarPorNome(busca);
-                            if (encontrados.isEmpty()){
-                                System.out.println("Usuario nao encontrado");
-                            } else {
-                                encontrados.forEach(u -> System.out.println("Encontrado" + u));
-                        }
-                    }else if (opcao == 4) {
-                        System.out.println("Encerrando...");
-                        break;
+            } else if (opcao == 3) {
+                System.out.println("Digite o nome para busca: ");
+                String busca = scanner.nextLine();
+                var encontrados = userDAO.buscarPorNome(busca);
+                if (encontrados.isEmpty()) {
+                    System.out.println("Usuario nao encontrado");
+                } else {
+                    encontrados.forEach(u -> System.out.println("Encontrado" + u));
+                }
+            } else if (opcao == 4) {
+                System.out.println("Encerrando...");
+                break;
+            } else if (opcao == 5){
+                userDAO.generateRandomUsers(2);
+                System.out.println("Usuerio gerado com sucesso");
+            } else if (opcao == 6) {
+                System.out.println("Digite o nome de usuario que deseja atualizar: ");
+                String novoNome = scanner.nextLine();
+
+                System.out.println("Digite o novo email: ");
+                String novoEmail = scanner.nextLine();
+
+                boolean atualizado = userDAO.updateUserAndEmail(novoNome,novoEmail);
+
+                if (!atualizado) {
+                    System.out.println("O nome de usuario não foi atualizado");
+                }
             } else {
-                        System.out.println("opcao invalida");
-                    }
-        }scanner.close();
+                System.out.println("opcao invalida");
+            }
+        }
+        scanner.close();
     }
 }
--- src/main/java/org/example/repository/UserDAO.java
+++ src/main/java/org/example/repository/UserDAO.java
@@ -1,8 +1,13 @@
-package org.example;
+package org.example.repository;
+
+import org.example.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class UserDAO {
     private List<User> usuarios = new ArrayList<>();
@@ -25,4 +30,26 @@ public class UserDAO {
                 .filter(u -> u.getNome().equalsIgnoreCase(nome))
                 .collect(Collectors.toList());
     }
-}
+
+    public void generateRandomUsers(int quantidade) {
+        Stream.generate(() -> {
+            String nome = "usuario" + new Random().nextInt(1000);
+            String email = nome.toLowerCase() + "@exemplo.com";
+            int idade = new Random().nextInt(100) + 1;
+            return (new User(nome, email, idade));
+        }).limit(quantidade)
+                .forEach(this::adicionar);
+
+    }
+
+    public Boolean updateUserAndEmail(String nome, String novoEmail) {
+        return usuarios.stream()
+                .filter(u -> u.getNome().equalsIgnoreCase(nome))
+                .findFirst()
+                .map(u -> {
+                    u.setEmail(novoEmail);
+                    return true;
+                }).orElse(false);
+    }
+        }
+
